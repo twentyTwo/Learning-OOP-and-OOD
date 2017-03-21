289c64652e05bba6a6503524e6a4e74067c64e60
Md. Noor Alam Shuvo <noor.alam.shuvo@gmail.com>
2017-03-21 12:17:10 +0600

    Add project files.

---
+++ StopWatch/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+
+namespace StopWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            Console.WriteLine("1 to start and 2 to stop");
+            while (true)
+            {
+                
+                int cmmd = int.Parse(Console.ReadLine());
+
+                if (cmmd == 1)
+                {
+                    try
+                    {
+                        stopwatch.Start();
+                    }
+                    catch (Exception)
+                    {
+
+                        Console.WriteLine("Stopwatch is running..");
+                    }
+                    
+                }
+                if (cmmd == 2)
+                {
+                    try
+                    {
+                        stopwatch.Stop();
+                        Console.WriteLine($"Duration is {stopwatch.Duration.Seconds} seconds");
+                        
+                    }
+                    catch (Exception)
+                    {
+
+                        Console.WriteLine("Stopwatch is stop.");
+                    }
+                    
+                }
+
+
+            }
+
+
+        }
+    }
+}
---
+++ StopWatch/Stopwatch.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace StopWatch
+{
+    class Stopwatch
+    {
+        private bool _isRunning;
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+        public TimeSpan Duration
+        {
+            get
+            {
+                return (EndTime - StartTime); 
+            }
+        }
+
+
+        public void Start()
+        {
+            if (_isRunning)
+            {
+                throw new InvalidOperationException("Stopwatch is running.");
+            }
+
+            _isRunning = true;
+            StartTime = DateTime.Now;          
+        }
+
+        public void Stop()
+        {
+            if (!_isRunning)
+            {
+                throw new InvalidOperationException("Stopwatch is stop.");
+            }
+            
+            _isRunning = false;
+            EndTime = DateTime.Now;
+            
+
+        }
+    }
+}
