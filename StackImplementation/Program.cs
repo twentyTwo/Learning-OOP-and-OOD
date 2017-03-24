4c869028ad8c78121b88df0065d4e44007c56125
Md. Noor Alam Shuvo <noor.alam.shuvo@gmail.com>
2017-03-24 12:58:21 +0600

    Add stackImplementation

---
+++ StackImplementation/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+namespace StackImplementation
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var stack = new Stack();
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("1 : Push, 2 : Pop, 3: Clear");
+                    var cmd = int.Parse(Console.ReadLine());
+
+                    switch (cmd)
+                    {
+                        case 1:
+                            Console.WriteLine("Enter value:");
+                            stack.Push(Console.ReadLine());
+                            PrintValues(stack);
+                            break;
+                        case 2:
+                            Console.WriteLine(stack.Pop());
+                            PrintValues(stack);
+                            break;
+                        case 3:
+                            stack.Clear();
+                            PrintValues(stack);
+                            break;
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Invalid operation.");
+                }
+            }
+        }
+
+        private static void PrintValues(Stack stack)
+        {
+            var list = stack.ViewStack().ToArray().Reverse();
+
+            Console.WriteLine("-----Stack-----");
+            foreach (var x in list)
+            {
+                Console.WriteLine(x);
+            }
+            Console.WriteLine("---------------");
+        }
+    }
+}
---
+++ StackImplementation/Stack.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+
+namespace StackImplementation
+{
+    public class Stack
+    {
+        private readonly ArrayList _list = new ArrayList();
+        private int _index = -1;
+
+        public void Push(object obj)
+        {
+            if (obj == null)
+            {
+                throw new InvalidOperationException("Null can not be pushed to Stack");
+            }
+            _list.Add(obj);
+            _index++;
+        }
+
+        public object Pop()
+        {
+            if (_index == -1)
+            {
+                throw new InvalidOperationException("Stack is empty");
+            }
+            var top = _list[_index];
+            _list.RemoveAt(_index);
+            _index--;
+            return top;
+        }
+
+        public void Clear()
+        {
+            _list.Clear();
+        }
+
+        public ArrayList ViewStack()
+        {
+            return _list;
+        }
+    }
+}
